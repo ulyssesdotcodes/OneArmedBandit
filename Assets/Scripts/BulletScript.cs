cd221b49e0a3e54530e12d8a681cdd832affa9bf
Ulysses Popple <ulysses.popple@gmail.com>
2013-02-09 12:37:51 -0500

    Bullets working

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,35 +2,49 @@ using UnityEngine;
 using System.Collections;
 
 public class BulletScript : MonoBehaviour {
-	public float bulletSpeed = 0.5f;
+	public float bulletSpeed = 50.0f;
 	Vector3 flattenedPlayerToCamera;
-	public GameObject bulletPrefab = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+	public GameObject bulletPrefab;
+	public Material bulletMat;
 	
+	private Plane playingPlane;
 	
 	// Use this for initialization
 	void Start () {
 		flattenedPlayerToCamera = this.gameObject.transform.position - Camera.main.transform.position;
 		flattenedPlayerToCamera.y = 0;
+		playingPlane = new Plane(Vector3.up, Vector3.zero);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Fire1")){
+		if(Input.GetButton("Fire1")){
 			Debug.Log("Shooting");
 			//Create the bullet
-			GameObject bullet = (GameObject) Instantiate(bulletPrefab);
+			GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			bullet.renderer.material = bulletMat;
 			bullet.AddComponent<Rigidbody>();
+			bullet.transform.position = this.gameObject.transform.position;
+			bullet.collider.isTrigger = true;
+			bullet.rigidbody.useGravity = false;
 			
 			//Find the vector from the camera to the mouse click position in the x-y plane
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			Vector3 flattenedRay = ray.direction;
-			flattenedRay.y = 0;
 			
-			//Find the vector from the this player transform to the place where the mouse clicked
-			Vector3 playerToMouseClick = flattenedRay + flattenedPlayerToCamera;
+			float dist;
+			playingPlane.Raycast(ray, out dist);
 			
-			//Add that vector to the bullet velocity
-			bullet.rigidbody.velocity = playerToMouseClick.normalized * bulletSpeed;
+			Vector3 flattenedPosition = this.gameObject.transform.position;
+			flattenedPosition.y = 0;
+			
+			
+			Vector3 playerToMouseClick = ray.GetPoint(dist) - flattenedPosition;
+			playerToMouseClick.Normalize();
+			playerToMouseClick *= bulletSpeed;
+			
+			Debug.Log("player to mouse " + (playerToMouseClick * bulletSpeed));
+		
+			bullet.rigidbody.velocity = playerToMouseClick;
 		}
 	}
 }
---
+++ Assets/Shaders/BulletShader.shader
@@ -0,0 +1,14 @@
+Shader "Custom/BulletShader" {
+	Properties {
+		_Color ("Main Color", Color) = (1,0.5,0.5,1)
+	}
+	SubShader {
+		Pass {
+			Material {
+				Diffuse[_Color]
+			}
+			Lighting On
+		}
+	} 
+	FallBack "Diffuse"
+}
